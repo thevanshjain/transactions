8308acb44c98146970d839dbb22b069396778896
Vansh <vanshvasu2810@gmail.com>
2022-03-27 12:33:31 +0530

    initial commit

---
+++ src/main/java/com/vansh/fakequeue/FakeQueueApplication.java
@@ -0,0 +1,13 @@
+package com.vansh.fakequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vansh/fakequeue/FakeQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vansh.fakequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
